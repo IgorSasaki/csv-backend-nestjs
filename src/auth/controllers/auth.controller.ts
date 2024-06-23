a4be79478f53d2fb0e9b60d4c49b19bbaa49e6aa
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-23 20:31:48 -0300

    feat(auth controllers auth.controller): implement service class to auth controller

---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common'
+
+import { CreateUserDto } from '../dto/create-user.dto'
+import { LoginUserDto } from '../dto/login-user.dto'
+import { AuthService } from '../services/auth.service'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto)
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto)
+  }
+}
