987ea852f1fb097d241daecfca1ebb4226a4d68f
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-23 20:25:05 -0300

    feat(users schemas user.schema): implement user schema for mongo database

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  userId: string
+
+  @Prop({ required: true, unique: true })
+  email: string
+
+  @Prop({ required: true })
+  password: string
+
+  @Prop({ required: true })
+  name: string
+
+  @Prop({ required: true })
+  createdAt: string
+
+  @Prop({ required: true })
+  updatedAt: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
