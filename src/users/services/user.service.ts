9d4716dbef1cb014dc17e81abf8afcee05ecffe5
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-23 20:24:27 -0300

    feat(users services user.service): implement service class to handle user

---
+++ src/users/services/user.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import * as bcrypt from 'bcrypt'
+import { Model } from 'mongoose'
+import { CreateUserDto } from 'src/auth/dto/create-user.dto'
+import { v4 as uuidv4 } from 'uuid'
+
+import { User } from '../schemas/user.schema'
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+
+  public async create(user: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(user.password, 10)
+    const dateNow = new Date().toISOString()
+
+    const existUser = await this.findOneByEmail(user.email)
+
+    if (existUser) {
+      throw new BadRequestException(
+        `User with email '${user.email}' already exist`
+      )
+    }
+
+    const newUser = new this.UserModel({
+      ...user,
+      userId: uuidv4(),
+      createdAt: dateNow,
+      updatedAt: dateNow,
+      password: hashedPassword
+    })
+
+    return newUser.save()
+  }
+
+  public async findOneByEmail(email: string) {
+    const user = await this.UserModel.findOne({ email }).exec()
+
+    if (!user) {
+      return null
+    }
+
+    return {
+      name: user.name,
+      userId: user.userId,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      password: user.password
+    }
+  }
+}
