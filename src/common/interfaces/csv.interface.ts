1bcc8737db150fd275d8c681dbe779a82d813ed7
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-24 01:44:18 -0300

    feat(common interfaces csv.interface): implement logic model to csv data

---
+++ src/common/interfaces/csv.interface.ts
@@ -0,0 +1,10 @@
+export interface ICsvData {
+  csvRowId: string
+  userId: string
+  email: string
+  name: string
+  age: number
+  monetaryValue: number
+  createdAt: string
+  updatedAt: string
+}
