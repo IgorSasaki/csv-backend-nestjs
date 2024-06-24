fcb0516e051c9f1914be3d9a548da6f65b4ce002
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-24 01:41:30 -0300

    feat(csv controllers csv.controller): update controller to integrate with database

--- src/csv/controllers/csv.controller.ts
+++ src/csv/controllers/csv.controller.ts
@@ -1,18 +1,20 @@
 import {
   BadRequestException,
   Controller,
+  Get,
   Post,
+  Req,
   UploadedFile,
   UseGuards,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { Express } from 'express';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
+  UseInterceptors
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { Request } from 'express'
+import { diskStorage } from 'multer'
+import { extname } from 'path'
 
-import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
-import { CsvService } from '../services/csv.service';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard'
+import { CsvService } from '../services/csv.service'
 
 @Controller('csv')
 export class CsvController {
@@ -28,32 +30,44 @@ export class CsvController {
           const randomName = Array(32)
             .fill(null)
             .map(() => Math.round(Math.random() * 16).toString(16))
-            .join('');
-          cb(null, `${randomName}${extname(file.originalname)}`);
-        },
+            .join('')
+          cb(null, `${randomName}${extname(file.originalname)}`)
+        }
       }),
       fileFilter: (req, file, cb) => {
         if (file.mimetype !== 'text/csv') {
           return cb(
             new BadRequestException('Only CSV files are allowed'),
-            false,
-          );
+            false
+          )
         }
-        cb(null, true);
+        cb(null, true)
       },
-      limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
-    }),
+      limits: { fileSize: 10 * 1024 * 1024 } // 10 MB
+    })
   )
-  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Req() req: Request
+  ) {
     if (!file) {
-      throw new BadRequestException('File is required');
+      throw new BadRequestException('File is required')
     }
+    const userId = req.user.userId
 
-    await this.csvService.processCsv(file);
+    await this.csvService.processCsv(file, userId)
 
     return {
       message: 'File uploaded successfully and queued for processing',
-      file: file.filename,
-    };
+      file: file.filename
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('data')
+  async getCsvData(@Req() req: Request) {
+    const userId = req.user.userId
+
+    return this.csvService.getCsvData(userId)
   }
 }
