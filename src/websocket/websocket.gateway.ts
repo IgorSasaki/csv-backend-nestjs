6352e2d7217b1f462dee8997101e51bd6be8cf0c
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-24 00:17:27 -0300

    feat(websocket): implement module and service class to handler websocket

---
+++ src/websocket/websocket.gateway.ts
@@ -0,0 +1,28 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
+
+@WebSocketGateway()
+export class WebSocketGatewayClass
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id)
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id)
+  }
+
+  emitCsvProcessed(data: any) {
+    console.log('Emitting csvProcessed event:', data)
+    this.server.emit('csvProcessed', data)
+  }
+}
---
+++ src/websocket/websocket.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+
+import { WebSocketGatewayClass } from './websocket.gateway'
+
+@Module({
+  providers: [WebSocketGatewayClass],
+  exports: [WebSocketGatewayClass]
+})
+export class WebSocketModule {}
