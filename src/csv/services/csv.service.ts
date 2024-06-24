61dab55e0e2c7466d917b57e1a928fa1987989f1
IgorSasaki <igor-sasaki@hotmail.com>
2024-06-24 00:18:01 -0300

    feat(csv services csv.service): implement service class to handler csv upload

---
+++ src/csv/services/csv.service.ts
@@ -0,0 +1,14 @@
+import { InjectQueue } from '@nestjs/bull'
+import { Injectable } from '@nestjs/common'
+import { Queue } from 'bull'
+
+@Injectable()
+export class CsvService {
+  constructor(@InjectQueue('csv') private readonly csvQueue: Queue) {}
+
+  public async processCsv(file: Express.Multer.File) {
+    await this.csvQueue.add('processCsv', {
+      filePath: file.path
+    })
+  }
+}
